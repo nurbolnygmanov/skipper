a86c3a4d33146820c981d09e225f9ef2e94b9291
nurbol <nurbolnygmanov@gmail.com>
2023-08-09 13:56:41 +0200

    feat(error boundary): add error boundary to app provider

--- src/providers/app-provider.tsx
+++ src/providers/app-provider.tsx
@@ -1,9 +1,16 @@
 import { ReactNode } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/react-query";
+import { ErrorBoundary } from "react-error-boundary";
 
 export function AppProvider({ children }: { children: ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary
+        fallback={<div>Error occured, please try later.</div>}
+        onError={console.error}
+      />
+      {children}
+    </QueryClientProvider>
   );
 }
