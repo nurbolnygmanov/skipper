73cbd82d75eb18b06be6e9a042491331bc48418f
nurbol <nurbolnygmanov@gmail.com>
2023-08-10 14:42:28 +0200

    feat(scss): add styling to dashboard layout

--- src/layouts/dashboard-layout.tsx
+++ src/layouts/dashboard-layout.tsx
@@ -8,30 +8,32 @@ import { useLogout } from "@/features/auth/api/logout";
 import styles from "./dashboard-layout.module.scss";
 import Image from "next/image";
 import logo from "../../public/logo-skipperndt.png";
+import profile from "../../public/profile.png";
+import { FiMessageSquare, FiBookmark, FiLogOut, FiMap } from "react-icons/fi";
 
 type DashboardLayoutProps = {
   children: ReactNode;
 };
 
-// export const DashboardLayout = ({ children }: DashboardLayoutProps) => {
-//   return (
-//     <Protected>
-//       <Header />
-//       <main>{children}</main>
-//     </Protected>
-//   );
-// };
-
 export function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>Header part</header>
-      <div className={styles.content}>
-        <nav className={styles.sidebar}>Navigation</nav>
+    <Protected>
+      <div className={styles.container}>
+        <Header />
+        <div className={styles.content}>
+          <nav className={styles.sidebar}>
+            <ul>
+              <li>
+                <FiMap size={22} color={"var(--color-grey-light-1)"} />{" "}
+                <span>INSPECTIONS</span>
+              </li>
+            </ul>
+          </nav>
 
-        <main className={styles.view}>{children}</main>
+          <main className={styles.view}>{children}</main>
+        </div>
       </div>
-    </div>
+    </Protected>
   );
 }
 
@@ -44,19 +46,26 @@ function Header() {
 
   return (
     <header className={styles.header}>
-      <Link href="/dashboard/inspections">
-        <Image
-          src={logo}
-          alt="skipper logo"
-          className={styles.logo}
-          width={193}
-          height={32}
-        />
-      </Link>
-      <nav className={styles.navmain}>
-        <p>{user.data?.name}</p>
+      <Image
+        src={logo}
+        alt="skipper logo"
+        className={styles.logo}
+        width={185}
+        height={28}
+      />
+
+      <nav className={styles.usernav}>
+        <p>
+          <FiMessageSquare size={22} color={"var(--color-grey-dark-2)"} />
+        </p>
+
+        <p>
+          <FiBookmark size={22} color={"var(--color-grey-dark-2)"} />
+        </p>
+
+        <span className={styles["user-name"]}>{user.data?.name}</span>
         <Button variant="outline" onClick={() => logout.submit()}>
-          Log Out
+          <FiLogOut size={22} color={"var(--color-grey-dark-2)"} /> Log out
         </Button>
       </nav>
     </header>
